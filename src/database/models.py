2a7940e54a9fe33bee16f5ce96990f23c1ccdaad
AndriiLiubov <andrii.liubov@gmail.com>
2025-03-19 19:00:45 +0200

    model,database, schema

---
+++ src/database/models.py
@@ -0,0 +1,31 @@
+from sqlalchemy import Column, Integer, String, func
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.schema import ForeignKey
+from sqlalchemy.sql.sqltypes import DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+class Note(Base):
+    __tablename__ = 'notes'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(50),nullable=False)
+    content = Column(String, nullable=False)
+    created_at = Column(DateTime, default=func.now())
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
+
+    versions = relationship('NoteVersion', back_populates='note', cascade="all, delete-orphan")
+
+
+class NoteVersion(Base):
+    __tablename__ = 'note_versions'
+
+    id = Column(Integer, primary_key=True)
+    note_id = Column(Integer, ForeignKey('notes.id'), nullable=False)
+    title = Column(String(50), nullable=False)
+    content = Column(String, nullable=False)
+    created_at = Column(DateTime, default=func.now())
+
+    note = relationship('Note', back_populates='versions')
---
+++ src/schemas.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from pydantic import BaseModel, Field
+
+
+class NoteModel(BaseModel):
+    title: str = Field(max_length=50)
+    content: str
+    created_at: datetime = Field(default_factory=datetime.now)
+    updated_at: datetime = Field(default_factory=datetime.now)
+
+class NoteResponse(NoteModel):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+class NoteVersionResponse(BaseModel):
+    id: int
+    note_id: int
+    title: str = Field(max_length=50)
+    content: str
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
